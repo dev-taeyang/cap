a7f8a8ca4c8f25ed6d410cd1e48a353f855147b7
dennisjeong <jiwook6273@gmail.com>
2023-03-17 01:56:24 +0900

    마이페이지 댓글, 보고서리스트, 탐험대 리스트 작업 및 마이페이지 수정 작업

---
+++ src/main/resources/static/js/mypage/mypageReview.js
@@ -0,0 +1,22 @@
+/* mypageReview.html */
+
+const $pageNumber = $(".page-number");
+const $prevButton = $(".prev-button img");
+const $prevPrevButton = $(".prev-prev-button img");
+const $nextButton = $(".next-button img");
+const $nextNextButton = $(".next-next-button img");
+
+// 페이지 수가 1보다 크다면 다음 버튼 활성화
+if($pageNumber.length > 1){
+    $nextButton.css("filter", "invert(1)");
+    $nextNextButton.css("filter", "invert(1)");
+    $nextButton.css("cursor", "pointer");
+    $nextNextButton.css("cursor", "pointer");
+}
+// 페이징 클릭 이벤트
+$pageNumber.each((i, e) => {
+    $(e).click(function(){
+        $pageNumber.removeClass("page-active");
+        $(e).addClass("page-active");
+    });
+});
