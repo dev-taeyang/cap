946ed88d403b7204c564188d961fa344910ce17b
dev-taeyang <oty7942@gmail.com>
2023-03-24 17:39:51 +0900

    reviewController 수정

--- src/main/java/com/app/captain/controller/ReviewController.java
+++ src/main/java/com/app/captain/controller/ReviewController.java
@@ -5,9 +5,7 @@ import com.app.captain.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,14 +17,46 @@ public class ReviewController {
 
     private final ReviewService reviewService;
 
+//    리뷰 상세보기
     @GetMapping("detail")
     public ReviewVO getReview(@PathVariable("reviewId") Long reviewId){
         return reviewService.getReveiw(reviewId);
     }
 
+//    리뷰 리스트
     @GetMapping("list")
     public List<ReviewVO> getList(){
+        log.info(reviewService.getList().toString());
         return reviewService.getList();
     }
+//    리뷰 작성
+    @GetMapping("register")
+    public void register(ReviewVO reviewVO){
+        reviewVO.setReviewCategory("여행");
+        reviewVO.setReviewContent("여행을 떠나요");
+        reviewVO.setReviewTitle("즉흥여행");
+        reviewVO.setGroupId(1L);
+        reviewVO.setReviewGrade(5D);
+        log.info(reviewVO.toString());
+        reviewService.write(reviewVO);
+    }
 
+//    리뷰 삭제
+    @PutMapping("delete/{reviewId}")
+    public void delete(@PathVariable("reviewId")Long reviewId){
+        log.info(reviewService.getReveiw(reviewId).toString());
+        reviewService.remove(reviewId);
+    }
+
+//    리뷰 수정
+    @GetMapping("modify/{reviewId}")
+    public void modify(@PathVariable("reviewId") Long reviewId){
+        ReviewVO reviewVO = reviewService.getReveiw(reviewId);
+        reviewVO.setReviewGrade(4.0D);
+        reviewVO.setReviewTitle("정지욱 바보");
+        reviewVO.setReviewContent("그만좀아파라");
+        reviewVO.setReviewCategory("원숭이");
+        log.info(reviewVO.toString());
+        reviewService.modify(reviewVO);
+    }
 }
