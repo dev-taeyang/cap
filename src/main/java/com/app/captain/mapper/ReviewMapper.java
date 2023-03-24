b49343301b2cf48d9f5f581b39318921f30da8fb
dev-taeyang <oty7942@gmail.com>
2023-03-24 16:12:42 +0900

    review 백엔드 작업 수정, admin css 수정

--- src/main/java/com/app/captain/mapper/ReviewMapper.java
+++ src/main/java/com/app/captain/mapper/ReviewMapper.java
@@ -1,8 +1,24 @@
 package com.app.captain.mapper;
 
-import lombok.RequiredArgsConstructor;
+import com.app.captain.domain.vo.ReviewVO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface ReviewMapper {
+//    리뷰 추가
+    public void insert(ReviewVO reviewVO);
+
+//    리뷰 수정
+    public void update(ReviewVO reviewVO);
+
+//    리뷰 조회
+    public ReviewVO select(Long reviewId);
+
+//    리뷰 전체 조회
+    public List<ReviewVO> selectAll();
+
+//    리뷰 삭제
+    public void delete(Long reviewId);
 }
--- src/main/java/com/app/captain/service/ReviewService.java
+++ src/main/java/com/app/captain/service/ReviewService.java
@@ -1,12 +1,43 @@
 package com.app.captain.service;
 
+import com.app.captain.domain.dao.ReviewDAO;
+import com.app.captain.domain.vo.ReviewVO;
 import com.app.captain.mapper.ReviewMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
 
-    private final ReviewMapper reviewMapper;
+    private final ReviewDAO reviewDAO;
+
+//    게시물 작성
+    public void write(ReviewVO reviewVO){
+        reviewDAO.save(reviewVO);
+    }
+
+//    게시물 수정
+    public void modify(ReviewVO reviewVO){
+        reviewDAO.setReviewVO(reviewVO);
+    }
+
+//    게시물 삭제
+    public void remove(Long reviewId){
+        reviewDAO.deleteById(reviewId);
+    }
+
+//    게시물 조회
+    public ReviewVO getReveiw(Long reviewId){
+        return reviewDAO.findById(reviewId);
+    }
+
+//    게시물 전체 조회
+    public List<ReviewVO> getList(){
+        return reviewDAO.findAll();
+    }
+
+
 }
