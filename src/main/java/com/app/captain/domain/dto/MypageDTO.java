607e83858f6f7d1b170256998a29be0abd691542
zerojun <test.zerojune@gmail.com>
2023-03-27 02:06:03 +0900

    mypage 값 가져오기 수정중, update, delete 쿼리 추가

--- src/main/java/com/app/captain/domain/dto/MypageDTO.java
+++ src/main/java/com/app/captain/domain/dto/MypageDTO.java
@@ -6,4 +6,25 @@ import org.springframework.stereotype.Component;
 @Component
 @Data
 public class MypageDTO {
+    private Long memberId;
+    private String memberIdentification;
+    private String memberPassword;
+    private String memberEmail;
+    private String memberName;
+    private String memberNickName;
+    private String memberPhone;
+    private String memberBirth;
+    private String memberGender;
+    private boolean memberStatus;
+    private String memberFileOriginalName;
+    private String memberFileUuid;
+    private String memberFilePath;
+    private String memberFileSize;
+    private boolean memberFileType;
+    private Long groupReplyId;
+    private String groupReplyContent;
+    private String groupReplyRegisterDate;
+    private String groupReplyUpdateDate;
+    private Long groupId;
+
 }
--- src/main/java/com/app/captain/mapper/MypageMapper.java
+++ src/main/java/com/app/captain/mapper/MypageMapper.java
@@ -1,7 +1,18 @@
 package com.app.captain.mapper;
 
+import com.app.captain.domain.dto.MypageDTO;
+import com.app.captain.domain.vo.MemberVO;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MypageMapper {
+//    내 정보들 가져오기
+    public MypageDTO select(Long memberId);
+
+//    내 정보 수정하기
+    public void update(MemberVO memberVO);
+
+//    회원탈퇴하기
+    public void delete(Long memberId);
+
 }
