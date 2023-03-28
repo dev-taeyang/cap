887fbfc20355176c73f68aee007f0f49779547e3
dev-taeyang <oty7942@gmail.com>
2023-03-29 02:01:31 +0900

    review파일 수정중

--- src/main/resources/static/js/reviews/reviewInsert.js
+++ src/main/resources/static/js/reviews/reviewInsert.js
@@ -2,7 +2,7 @@ FileList.prototype.forEach = Array.prototype.forEach;
 globalThis.arrayFile = new Array();
 globalThis.i = 0;
 
-$("input[name='file']").on("change", function(){
+$("input[name='file']").on("change", function () {
     const $files = $("input[name=file]")[0].files;
     let formData = new FormData();
 
@@ -14,18 +14,24 @@ $("input[name='file']").on("change", function(){
         formData.append("file", file)
     });
     $.ajax({
-        url: "/reviews/write",
+        url: "/reviews/upload",
         type: "post",
         data: formData,
         contentType: false,
         processData: false,
-        success: function(uuids) {
+        success: function (uuids) {
             globalThis.uuids = uuids;
             $files.forEach((file, i) => {
-                if(file.type.startsWith("image")){
-                    $("#thumbnail").append(`<li><a href="/files/download?fileName=${toStringByFormatting(new Date())}/${uuids[i]}_${file.name}"><img src="/files/display?fileName=${toStringByFormatting(new Date())}/t_${uuids[i]}_${file.name}"></a></li>`);
-                }else{
-                    $("#thumbnail").append(`<li><a href="/files/download?fileName=${toStringByFormatting(new Date())}/${uuids[i]}_${file.name}"><img src="/attach.png" width="100"></a></li>`);
+                if (file.type.startsWith("image")) {
+                    $("#thumbnail").append(`
+                                             <div class="thumbnail"  >
+                                                <span class="thumbnailSpan" >
+                                                    <img src="/reviews/display?fileName=${toStringByFormatting(new Date())}/t_${uuids[i]}_${file.name}">
+                                                </span>
+                                             </div>
+                                          `);
+                } else {
+                    $("#thumbnail").append(`<div class="thumbnail"><span class="thumbnailSpan"><img src="/images/camera_icon.png" width="100"></span></div>`);
                 }
             });
             /********************************************************************/
@@ -46,7 +52,24 @@ $("input[name='file']").on("change", function(){
                     `
                 i++;
             });
-            $("form[name='write-form']").append(text);
+            $("form[name='reveiw']").append(text);
+            console.log("filse:" + files);
         }
     });
 });
+
+function leftPad(value) {
+    if (value >= 10) {
+        return value;
+    }
+
+    return `0${value}`;
+}
+
+function toStringByFormatting(source, delimiter = '/') {
+    const year = source.getFullYear();
+    const month = leftPad(source.getMonth() + 1);
+    const day = leftPad(source.getDate());
+
+    return [year, month, day].join(delimiter);
+}
