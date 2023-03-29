270d0da8a20d33587d3f2325e52e93986c5e17c5
zerojun <test.zerojune@gmail.com>
2023-03-29 17:54:14 +0900

    mypage 업데이트 수정

--- src/main/java/com/app/captain/controller/MypageController.java
+++ src/main/java/com/app/captain/controller/MypageController.java
@@ -6,12 +6,21 @@ import com.app.captain.service.MemberService;
 import com.app.captain.service.MypageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 @Controller
@@ -78,17 +87,50 @@ public class MypageController {
 
     //    내 정보 수정하기
     @PostMapping("Update")
-    public String modify(MemberVO memberVO) {
+    @ResponseBody
+    public void modify(@RequestBody MemberVO memberVO) {
         mypageService.modify(memberVO);
-        log.info(memberVO.toString());
-        return "redirect:/mypage/me";
     }
-    /* 회원탈퇴하기 */
+
+    //    파일 업로드
+    @PostMapping("upload")
+    @ResponseBody
+    public String upload(@RequestParam("memberFile") MultipartFile multipartFile) throws IOException {
+        String uuids = UUID.randomUUID().toString();
+        String path = "C:/upload/" + getPath();
+        File file = new File(path);
+        log.info(path + "/" + "t_" + uuids + "_" + multipartFile.getOriginalFilename());
+        if(!file.exists()) {file.mkdirs();}
+        multipartFile.transferTo(new File(path, uuids + "_" + multipartFile.getOriginalFilename()));
+
+        if(multipartFile.getContentType().startsWith("image")){
+            FileOutputStream out = new FileOutputStream(new File(path, "t_" + uuids + "_" + multipartFile.getOriginalFilename()));
+                Thumbnailator.createThumbnail(multipartFile.getInputStream(), out, 100, 100);
+            out.close();
+        }
+        return uuids;
+    }
+
+    //    파일 불러오기
+    @GetMapping("display")
+    @ResponseBody
+    public byte[] display(String fileName) throws IOException {
+        log.info(fileName.toString());
+        return FileCopyUtils.copyToByteArray(new File("C:/upload", fileName));
+    }
+
+    //    현재 날짜 경로 구하기
+    private String getPath(){
+        return LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
+    }
+
+    /* 회원탈퇴 폼으로 이동 */
     @GetMapping("remove")
     public String getRemove() {
         return "mypage/mypageUserLeave";
     }
 
+    /* 회원탈퇴 */
     @PostMapping("remove")
     public String memberRemove(HttpSession session) {
         MemberVO memberVO = (MemberVO) session.getAttribute("member");
