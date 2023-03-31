c20a7eb5e5d546bacb346552fc12c63b4a064ef1
dennisjeong <jiwook6273@gmail.com>
2023-03-31 16:05:14 +0900

    네이버 카카오 멤버 수정

--- src/main/resources/static/js/member/findId.js
+++ src/main/resources/static/js/member/findId.js
@@ -12,6 +12,8 @@ const checkIdButton = document.querySelector('.check-id-btn');
 const phoneRegex = /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/;
 let code;
 
+$('.modal').hide();
+
 $(inputPhone).on('blur', function () {
   let value = $(this).val();
   $(this).val(value.replace(/^(\d{2,3})(\d{3,4})(\d{4})$/, `$1-$2-$3`));
@@ -38,7 +40,7 @@ function activePhone() {
 inputCode.addEventListener('keyup', activeCode);
 
 function activeCode() {
-    switch ($(inputCode).val() == code) {
+    switch ($(inputCode).val().length == 4) {
       case true:
         codeButton.disabled = false;
         break;
@@ -49,11 +51,21 @@ function activeCode() {
   }
 
 $(codeButton).on('click', function(){
+    let modalMessage = "";
+    if($(inputCode).val() != code){
+        modalMessage = "인증 번호를 확인하세요.";
+        showWarnModal(modalMessage);
+        return;
+    }
+    modalMessage = "인증이 완료되었습니다.";
+    showWarnModal(modalMessage);
     checkIdButton.disabled = false;
 });
 
 $(phoneButton).on('click', function () {
     let phone = $(".join-phone").val().replaceAll("-", "");
+    let modalMessage = "인증번호가 전송되었습니다.";
+    showWarnModal(modalMessage);
     console.log(phone);
     $.ajax({
       type: "POST",
@@ -101,3 +113,22 @@ function goCheck() {
     300
   );
 }
+
+let modalCheck;
+function showWarnModal(modalMessage) {
+    modalCheck = false;
+    $('div.modal-content').html(modalMessage);
+    $('div.warn-modal').css('animation', 'popUp 0.5s');
+    $('div.modal').css('display', 'flex').hide().fadeIn(500);
+    setTimeout(function () {
+        modalCheck = true;
+    }, 500);
+}
+
+$('.find-id-container').on('click', function () {
+    if (modalCheck) {
+        $('div.warn-modal').css('animation', 'popDown 0.5s');
+        $('div.modal').fadeOut(500);
+    }
+});
+
