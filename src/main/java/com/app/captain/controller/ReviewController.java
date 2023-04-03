721ccd583e6b1a5055de45892863e458be5c3fcd
dev-taeyang <oty7942@gmail.com>
2023-04-03 23:57:17 +0900

    review QA 완료, review 인설트할때, 첫번째 이미지를 ReviewFileRep =1 나머지 0 수정

--- src/main/java/com/app/captain/controller/ReviewController.java
+++ src/main/java/com/app/captain/controller/ReviewController.java
@@ -54,28 +54,31 @@ public class ReviewController {
 
     //    리뷰 작성
     @PostMapping("write")
-    public RedirectView save(ReviewFileDTO reviewFileDTO, HttpSession session ,RedirectAttributes redirectAttributes) {
+    public RedirectView save(ReviewFileDTO reviewFileDTO, HttpSession session, RedirectAttributes redirectAttributes) {
         /* session에 딤긴 memberId 받아옴*/
-        Long memberId = (Long)session.getAttribute("memberId");
+        Long memberId = (Long) session.getAttribute("memberId");
         ReviewVO reviewVO = reviewFileDTO.toVO();
         /* 작성할때 로그인한 사람이 작성하게끔 memberId를 set해줌*/
         reviewVO.setMemberId(memberId);
         /* 리뷰작성*/
         reviewService.write(reviewVO);
-        if(reviewFileDTO.getFiles()!= null) {
+        if (reviewFileDTO.getFiles() != null) {
             List<ReviewFileVO> files = reviewFileDTO.getFiles();
             /* 저장된 파일들의 reviewId가 null이기때문에  파일들에 각각 review를 넣어준다*/
-            files.forEach(file -> file.setReviewId(reviewVO.getReviewId()));
+            files.forEach((file) -> {
+                file.setReviewId(reviewVO.getReviewId());
+            });
             /* 파일 저장*/
+            log.info(files.toString());
             reviewFileService.write(files);
         }
         redirectAttributes.addFlashAttribute("작성완료");
         return new RedirectView("/reviews/list");
     }
 
-//    리뷰 수정 페이지
+    //    리뷰 수정 페이지
     @GetMapping("{reviewId}/modify")
-    public String getModify(@PathVariable("reviewId")Long reviewId, Model model){
+    public String getModify(@PathVariable("reviewId") Long reviewId, Model model) {
         /* 기존 작성했던 정보를 가져와서 수정페이지에 보내줌 -> 기존값을 뿌려놓기 위해 */
         ReviewVO reviewVO = reviewService.getReview(reviewId);
         List<ReviewFileVO> files = new ArrayList<>();
@@ -88,9 +91,9 @@ public class ReviewController {
         return "reviews/reviewModify";
     }
 
-//    리뷰 수정 완료 페이지
+    //    리뷰 수정 완료 페이지
     @PostMapping("{reviewId}/modify")
-    public RedirectView modify(ReviewFileDTO reviewFileDTO, @PathVariable("reviewId") Long reviewId, RedirectAttributes redirectAttributes){
+    public RedirectView modify(ReviewFileDTO reviewFileDTO, @PathVariable("reviewId") Long reviewId, RedirectAttributes redirectAttributes) {
         ReviewVO reviewVO = reviewFileDTO.toVO();
         reviewVO.setReviewId(reviewId);
         /* 리뷰 수정*/
@@ -109,7 +112,7 @@ public class ReviewController {
     @GetMapping("detail/{reviewId}")
     public String getReview(@PathVariable("reviewId") Long reviewId, Model model, HttpSession session) {
         /* 세션에 담겨있는 memberId를 가져와서 변수에 담아줌 */
-        Long sessionId = (Long)session.getAttribute("memberId");
+        Long sessionId = (Long) session.getAttribute("memberId");
         /* reviewId로 조회해온 review 정보를 reviewFIleDTO로 DTO타입으로 바꿔줌 */
         ReviewFileDTO reviewFileDTO = reviewService.getReview(reviewId).toDTO();
         /* detail페이지에 담길 memberId와 session의 memberId와 비교하기 위해 추가로 addAttribute해줌 */
@@ -118,10 +121,10 @@ public class ReviewController {
         reviewFileDTO.setFiles(reviewFileService.getList(reviewId));
         /* 작성자의 정보를 뿌리기위해 memberVO정보를 가져옴 */
         MemberVO memberVO = mypageService.getMemberById(memberId);
-        model.addAttribute("memberVO",memberVO);
+        model.addAttribute("memberVO", memberVO);
         model.addAttribute("review", reviewFileDTO);
         model.addAttribute("reviewId", reviewId);
-        model.addAttribute("sessionId",sessionId);
+        model.addAttribute("sessionId", sessionId);
         return "reviews/reviewDetail";
     }
 
@@ -129,7 +132,7 @@ public class ReviewController {
     @GetMapping("list")
     public String getList(Model model, HttpSession session, Criteria criteria) {
         /*세션아이디 가져와서 보내주기*/
-        Long memberId = (Long)session.getAttribute("memberId");
+        Long memberId = (Long) session.getAttribute("memberId");
         /*Review와reviewFile 조인한 DTO 타입의 ArrayList를 선언*/
         List<ReviewFileDTO> reviewFileDTOS = new ArrayList<>();
         /* 유저 정보들을 담기위해 MemberVO타입의 ArrayList 선언*/
@@ -149,18 +152,18 @@ public class ReviewController {
             reviewFileDTO.setFiles(reviewFileService.getList(reviewId));
         });
         /*model 객체를 통해 reviewFileDTOS를 reviews라는 키 name으로 view에 전달해줌*/
-        model.addAttribute("criteria",criteria);
+        model.addAttribute("criteria", criteria);
         model.addAttribute("reviews", reviewFileDTOS);
-        model.addAttribute("memberId",memberId);
+        model.addAttribute("memberId", memberId);
         return "reviews/reviewList";
     }
 
-//    리뷰 삭제
+    //    리뷰 삭제
     @GetMapping("{reviewId}/remove")
-    public RedirectView remove(@PathVariable("reviewId")Long reviewId, RedirectAttributes redirectAttributes){
+    public RedirectView remove(@PathVariable("reviewId") Long reviewId, RedirectAttributes redirectAttributes) {
         /* reviewId로 delete Cascade*/
         reviewService.remove(reviewId);
-        redirectAttributes.addFlashAttribute("reviewId","삭제완료");
+        redirectAttributes.addFlashAttribute("reviewId", "삭제완료");
         return new RedirectView("/reviews/list");
     }
 
@@ -192,12 +195,12 @@ public class ReviewController {
         return uuids;
     }
 
-//    파일 불러오기
+    //    파일 불러오기
     @GetMapping("display")
     @ResponseBody
-    public byte[] display(String fileName) throws IOException{
+    public byte[] display(String fileName) throws IOException {
         /* 실제 파일 경로에있는 파일을 byte 배열로 불러와줌*/
-        return FileCopyUtils.copyToByteArray(new File("C:/upload",fileName));
+        return FileCopyUtils.copyToByteArray(new File("C:/upload", fileName));
     }
 
 }
