fb0611fc557743502cc02fecce52214c8a33feb3
dennisjeong <jiwook6273@gmail.com>
2023-04-03 11:24:24 +0900

    오류 페이지, 헤더 푸터 연결, 공지사항, FAQ 수정

--- src/main/java/com/app/captain/controller/AdminController.java
+++ src/main/java/com/app/captain/controller/AdminController.java
@@ -1,6 +1,11 @@
 package com.app.captain.controller;
 
+import com.app.captain.domain.dto.Criteria;
+import com.app.captain.domain.dto.Search;
+import com.app.captain.service.MemberService;
 import com.app.captain.service.NoticeService;
+import com.app.captain.service.ReviewFileService;
+import com.app.captain.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -17,12 +22,24 @@ import javax.annotation.Resource;
 @Slf4j
 @RequiredArgsConstructor
 public class AdminController {
+    private final MemberService memberService;
     private final NoticeService noticeService;
+    private final ReviewService reviewService;
+    private final ReviewFileService reviewFileService;
 
     @GetMapping("/admin-notice")
-    public void showList(Model model, Long noticeId){
+    public void showNotices(Model model, Long noticeId){
         model.addAttribute("notices", noticeService.getList());
         model.addAttribute("noticeCount", noticeService.getNoticeCount());
         log.info("notice");
+    }
+
+    @GetMapping("/admin-member")
+    public void showMembers(Model model, Long memberId){
+    }
+
+    @GetMapping("/admin-review")
+    public void showReviews(Model model, Criteria criteria, Search search){
+        model.addAttribute("reviews", reviewService.getList(criteria, search));
     }
 }
---
+++ src/main/java/com/app/captain/controller/ExceptionController.java
@@ -0,0 +1,12 @@
+package com.app.captain.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+public class ExceptionController {
+
+    /*@RequestMapping("/error")
+    public RedirectView handle(Exception e){
+        return new RedirectView("/error/404");
+    }*/
+}
