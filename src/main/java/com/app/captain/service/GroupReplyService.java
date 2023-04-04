0e7c8736a64d7e9b2e2282c0b741cf1541d50d23
zerojun <test.zerojune@gmail.com>
2023-04-04 23:45:02 +0900

    group reply mapper, dao, xml, service 수정 controller 추가

--- src/main/java/com/app/captain/service/GroupReplyService.java
+++ src/main/java/com/app/captain/service/GroupReplyService.java
@@ -1,9 +1,13 @@
 package com.app.captain.service;
 
 import com.app.captain.domain.dao.GroupReplyDAO;
+import com.app.captain.domain.dto.GroupReplyDTO;
+import com.app.captain.domain.vo.GroupReplyVO;
 import com.app.captain.mapper.GroupReplyMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,4 +17,24 @@ public class GroupReplyService {
     /* 그룹 id로 탐험대에 작성된 댓글의 갯수 가져오기 */
     public Long getReplyCount(Long groupId) { return groupReplyDAO.findReplyCount(groupId); }
 
+
+    /* 그룹 id로 탐험대에 작성된 댓글들의 정보 가져오기 */
+    public List<GroupReplyDTO> getGroupReply(Long groupId) {
+        return groupReplyDAO.findGroupReply(groupId);
+    }
+
+    /* 댓글 입력하기 */
+    public void setReply(GroupReplyVO groupReplyVO) {
+        groupReplyDAO.saveReply(groupReplyVO);
+    }
+
+    /* 댓글 삭제하기 */
+    public void removeReply(Long groupReplyId) {
+        groupReplyDAO.deleteReply(groupReplyId);
+    }
+
+    /* 댓글 수정하기 */
+    public void modifyReply(GroupReplyVO groupReplyVO) {
+        groupReplyDAO.setReplyVO(groupReplyVO);
+    }
 }
