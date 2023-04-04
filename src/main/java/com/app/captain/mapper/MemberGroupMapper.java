1c2691b3565527b366186750d64f248ed568ede8
dev-taeyang <oty7942@gmail.com>
2023-04-04 16:16:44 +0900

    그룹 수정 중

--- src/main/java/com/app/captain/mapper/MemberGroupMapper.java
+++ src/main/java/com/app/captain/mapper/MemberGroupMapper.java
@@ -5,4 +5,16 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MemberGroupMapper {
+
+    /* 그룹 참여하기 */
+    public void join(Long groupId, Long memberId);
+
+    /* 그룹아이디로 memberGroupId 조회하기 */
+    public Long selectByGroupId(Long groupId);
+
+    /* 그룹아이디로 가입한 member 수 구하기 */
+    public int memberCountByGroupId(Long groupId);
+
+    /* groupId로 삭제하기 */
+    public void delete(Long groupId);
 }
