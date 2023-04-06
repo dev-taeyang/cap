1a23d129aec0a80539faf7e94e2e011f5d86bfd1
zerojun <test.zerojune@gmail.com>
2023-04-06 20:31:17 +0900

    exception controller, mypage 성별 update

--- src/main/java/com/app/captain/controller/ExceptionController.java
+++ src/main/java/com/app/captain/controller/ExceptionController.java
@@ -5,8 +5,8 @@ import org.springframework.web.servlet.view.RedirectView;
 
 public class ExceptionController {
 
-    /*@RequestMapping("/error")
+    @RequestMapping("/error")
     public RedirectView handle(Exception e){
         return new RedirectView("/error/404");
-    }*/
+    }
 }
